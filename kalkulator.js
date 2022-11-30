99f721b93a7ecedfb13e1759badca0344d80582d
AmmarFarrasH <118705994+AmmarFarrasH@users.noreply.github.com>
2022-11-30 10:22:14 +0700

    Update kalkulator.js

--- kalkulator.js
+++ kalkulator.js
@@ -1,37 +1,37 @@
-var panggiluntukinput = require("readline-sync");
+const input = require("readline-sync");
+console.log("=====================================");
 console.log("Kalkulator Sederhana");
+console.log("=====================================");
+
+const a = input.questionInt("Masukkan nilai pertama : ");
+const b = input.questionInt("Masukan nilai kedua : ");
+const nilai1 = Number(a);
+const nilai2 = Number(b);
+
+console.log("=====================================");
 console.log("1. Penjumlahan (+)");
 console.log("2. Pengurangan (-)");
 console.log("3. Perkalian (*)");
 console.log("4. Pembagian (/)");
-var pilih = panggiluntukinput.questionInt("Pilihlah salah satu nomor di atas ini: ");
+const pilih = input.questionInt("Pilihlah salah satu nomor di atas ini: ");
+console.log("=====================================");
 
 if (pilih == 1){
-    var a = panggiluntukinput.questionInt("Masukkan nilai pertama: ");
-    var b = panggiluntukinput.questionInt("Masukkan nilai kedua: ");
-    var nilai1 = Number(a);
-    var nilai2 = Number(b);
+    console.log("Hasil dari " + nilai1 + (" ") + ("+") + (" ") + nilai2 + (" adalah"))
     hasil = nilai1 + nilai2
-    console.log(hasil);
+    
 }else if (pilih == 2){
-    var a = panggiluntukinput.questionInt("Masukkan nilai pertama: ");
-    var b = panggiluntukinput.questionInt("Masukkan nilai kedua: ");
-    var nilai1 = Number(a);
-    var nilai2 = Number(b);
+    console.log("Hasil dari " + nilai1 + (" ") + ("-") + (" ") + nilai2 + (" adalah"))
     hasil = nilai1 - nilai2
-    console.log(hasil);
+    
 }else if (pilih == 3){
-    var a = panggiluntukinput.questionInt("Masukkan nilai pertama: ");
-    var b = panggiluntukinput.questionint("Masukkan nilai kedua: ");
-    var nilai1 = Number(a);
-    var nilai2 = Number(b);
+    console.log("Hasil dari " + nilai1 + (" ") + ("*") + (" ") + nilai2 + (" adalah"))
     hasil = nilai1 * nilai2
-    console.log(hasil);
+    
 }else if (pilih == 4){
-    var a = panggiluntukinput.questionInt("Masukkan nilai pertama: ");
-    var b = panggiluntukinput.questionInt("Masukkan nilai kedua: ");
-    var nilai1 = Number(a);
-    var nilai2 = Number(b);
+    console.log("Hasil dari " + nilai1 + (" ") + ("/") + (" ") + nilai2 + (" adalah"))
     hasil = nilai1 / nilai2
-    console.log(hasil);
 }
+
+console.log(hasil)
+console.log("=====================================");
